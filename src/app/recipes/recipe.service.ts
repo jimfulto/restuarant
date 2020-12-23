031c7bd97282162c3fe37c514ebc842ffaeced28
Jim Fulto <jimfulto233@gmail.com>
2020-12-22 21:15:50 -0600

    added shopping-list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe ('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/11/16/18/51/kagyana-2955466_1280.jpg'),
-        new Recipe ('Another Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/11/16/18/51/kagyana-2955466_1280.jpg')
+        new Recipe (
+            'Chicken Biryani', 
+            'A super-tasty Chicken Biryani - just awesome', 
+            'https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            [
+                new Ingredient('Chicken', 1),
+                new Ingredient('Rice', 1)
+            ]),
+        new Recipe (
+            'Dumpling', 
+            'Delicious dumpling', 
+            'https://images.pexels.com/photos/4202392/pexels-photo-4202392.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            [
+                new Ingredient('Flour', 1),
+                new Ingredient('Milk', 1)
+            ])
       ];
 
       getRecipes() {
