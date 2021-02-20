bb1898bb4c57bb254e85167e04ee76f749a24e14
Jim Fulto <jimfulto233@gmail.com>
2021-02-20 00:22:06 -0600

    added template driven form for shopping-list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,7 @@ import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
     ingredients: Ingredient[] = [
         new Ingredient('Apple', 5),
         new Ingredient('Tomatoes', 10)
@@ -11,6 +12,10 @@ export class ShoppingListService {
 
       getIngredients() {
           return this.ingredients.slice();
+      }
+
+      getIngredient(index: number) {
+        return this.ingredients[index];
       }
 
       addIngredient(ingredient: Ingredient) {
@@ -21,6 +26,16 @@ export class ShoppingListService {
       //to push all ingredients together in one event we use spread operator
       addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+
+      updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+
+      deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
         this.ingredientsChanged.next(this.ingredients.slice());
       }
 }
